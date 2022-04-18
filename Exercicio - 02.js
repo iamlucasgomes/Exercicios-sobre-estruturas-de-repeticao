7c54675b18d0657fa8acaaf08de26b30982a2504
Lucas Gomes <lucas.devjs@gmail.com>
2022-04-17 22:18:18 -0300

    Add files via upload

--- Exercicio - 02.js
+++ Exercicio - 02.js
@@ -1,10 +1,13 @@
 //	2. Modifique o algoritmo anterior, de maneira que o usuário também informe o início e o fim da tabuada. Por exemplo: usuário solicitando a tabuada do número 6, com início em 5 e fim em 8.
 
-function tabuadaMultiplicacao(numeros,multiplicador){ // criei um função e determinei 2 parâmetros para ela, a lista de numeros e o multiplicador
-      
+function tabuadaMultiplicacao(numeros, multiplicador) {
+    // criei um função e determinei 2 parâmetros para ela, a lista de numeros e o multiplicador
+  
     var resultado = []; // criei uma variável com um array vazio para receber o resultado da nossa estrutura de repetição
-for (var indice = 0; indice < numeros.length; indice ++){ // usei uma estrutura de repetição onde vamos repetir a operação abaixo ate o limite determinado por numeros.length
+    for (var indice = 0; indice < numeros.length; indice++) {
+      // usei uma estrutura de repetição onde vamos repetir a operação abaixo ate o limite determinado por numeros.length
       resultado.push(numeros[indice] * multiplicador); //	multiplicamos cada elemento do array numeros pela variável multiplicador e  usamos o .push para adicionar elementos ao array resultado
-}
-return resultado; // declaramos que deve retornar a variável resultado quando usarmos a função
-}
+    }
+    return resultado; // declaramos que deve retornar a variável resultado quando usarmos a função
+  }
+  
--- Exercicio - 04.js
+++ Exercicio - 04.js
@@ -1,14 +1,16 @@
 // 4. Agora modifique o exercício anterior, considerando agora somente os resultados que forem ímpares.
 
-function tabuadaImpar(numeros,  multiplicador){	// criei uma função que vai receber 2 paramêtros, um deles é a array numeros onde vamos inserir os numeros que queremos multiplicar pelo multiplicador
-           
-    var resultado = []; // criei uma variavél com um array vazio e dei o nome de resultado, iremos colocar os numeros impares nessa array
-      
-    for (var indice = 0; indice < numeros.length; indice ++){ // usei a estrutura de repetição que vai verificar todos os elementos da array numeros a partir do seu primeiro elemento
-            if (numeros[indice] * multiplicador % 2 !== 0){ //	e se(if) o elemento da array numeros for multiplicado pelo multiplicador e tiver o resto diferente de 0
-              resultado.push(numeros[indice]*multiplicador)//	iremos adicionar ele na array resultado com o método push  
-        }
-        
-      } return resultado	//declarei o retorno da variável resultado quando usarmos a função
+function tabuadaImpar(numeros, multiplicador) {
+  // criei uma função que vai receber 2 paramêtros, um deles é a array numeros onde vamos inserir os numeros que queremos multiplicar pelo multiplicador
+
+  var resultado = []; // criei uma variavél com um array vazio e dei o nome de resultado, iremos colocar os numeros impares nessa array
+
+  for (var indice = 0; indice < numeros.length; indice++) {
+    // usei a estrutura de repetição que vai verificar todos os elementos da array numeros a partir do seu primeiro elemento
+    if ((numeros[indice] * multiplicador) % 2 !== 0) {
+      //	e se(if) o elemento da array numeros for multiplicado pelo multiplicador e tiver o resto diferente de 0
+      resultado.push(numeros[indice] * multiplicador); //	iremos adicionar ele na array resultado com o método push
     }
-    
+  }
+  return resultado; //declarei o retorno da variável resultado quando usarmos a função
+}
